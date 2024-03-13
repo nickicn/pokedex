b56f05202a382731d56519a768b55d8172cd3dc2
nickicn <nicolelopes0008@gmail.com>
2024-03-13 14:45:02 -0300

    143 (RESOLVER PROBLEMA ^^)

---
+++ Pokedex/Models/DetailsVm.cs
@@ -0,0 +1,10 @@
+namespace Pokedex.Models;
+
+public class DetailsVM
+
+{
+    public Pokemon Anterior { get; set; }
+     public Pokemon Atual { get; set; } 
+     public Pokemon Proximo { get; set; }
+      public Pokemon Tipos { get; set; }
+}
